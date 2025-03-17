36a6be32f14e5f117dc74848e1b0f945c2e72966
evilbc <pawel.jastrzebski01@gmail.com>
2025-03-17 16:09:39 +0100

    optimize Divide Array Into Equal Pairs

--- divide-array-into-equal-pairs/src/main/java/org/example/Solution.java
+++ divide-array-into-equal-pairs/src/main/java/org/example/Solution.java
@@ -1,41 +1,24 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 public class Solution {
+    private final int MAX_NUMBER = 500;
+
     public boolean divideArray(int[] nums) {
         if (nums.length % 2 != 0)
             return false;
 
-        Set<Integer> alreadyPaired = new HashSet<>();
+        int[] counts = new int[MAX_NUMBER];
 
         for (int i = 0; i < nums.length; i++) {
-            if (alreadyPaired.contains(i))
-                continue;
+            int num = nums[i];
+            counts[num - 1]++;
+        }
 
-            int pair = findPair(nums, i, alreadyPaired);
-            if (pair == -1)
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] % 2 != 0)
                 return false;
-
-            alreadyPaired.add(pair);
         }
 
         return true;
-    }
-
-    private int findPair(int[] nums, int index, Set<Integer> toSkip) {
-        for (int j = index + 1; j < nums.length; j++) {
-            if (toSkip.contains(j))
-                continue;
-
-            if (nums[j] == nums[index]) {
-                return j;
-            }
-        }
-        return -1;
-
     }
 }
