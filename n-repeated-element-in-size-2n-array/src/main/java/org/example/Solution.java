e6509450704649dc85feb73ee8ef7b5d0cb0bdda
evilbc <pawel.jastrzebski01@gmail.com>
2025-03-17 17:20:53 +0100

    add N-Repeated Element in Size 2N Array

---
+++ n-repeated-element-in-size-2n-array/src/main/java/org/example/Solution.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.*;
+
+public class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num))
+                return num;
+            set.add(num);
+        }
+        throw new IllegalStateException();
+    }
+}
