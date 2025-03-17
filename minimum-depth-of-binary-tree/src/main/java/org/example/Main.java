b6b4a6898c1d88702b646a4d665ce5c551665d15
evilbc <pawel.jastrzebski01@gmail.com>
2025-03-17 16:47:37 +0100

    add Minimum Depth of Binary Tree

---
+++ minimum-depth-of-binary-tree/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        boolean solution = new Solution().isValid("()");
+//        System.out.println(solution);
+    }
+
+}
