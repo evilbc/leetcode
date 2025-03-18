6529ad539b7fb6abc676c430c7926f0df36bf9e7
evilbc <pawel.jastrzebski01@gmail.com>
2025-03-18 12:48:55 +0100

    add 141. Linked List Cycle

---
+++ v2/src/main/java/org/example/_141/linked/list/cycle/ListNode.java
@@ -0,0 +1,11 @@
+package org.example._141.linked.list.cycle;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
---
+++ v2/src/main/java/org/example/_141/linked/list/cycle/Solution.java
@@ -0,0 +1,17 @@
+package org.example._141.linked.list.cycle;
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+}
