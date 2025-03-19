a60f53411775f4b73f20b67e8a5328d1a5aaf5ef
evilbc <pawel.jastrzebski01@gmail.com>
2025-03-19 15:23:06 +0100

    add 226. Invert Binary Tree

---
+++ v2/src/main/java/org/example/_226/invert/binary/tree/Solution.java
@@ -0,0 +1,17 @@
+package org.example._226.invert.binary.tree;
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
