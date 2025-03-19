6cb0a2a655927bf11388be6cd7669dd1f0c8074c
evilbc <pawel.jastrzebski01@gmail.com>
2025-03-19 13:40:21 +0100

    optimize 11. Container With Most Water

--- v2/src/main/java/org/example/_11/container/with/most/water/Solution.java
+++ v2/src/main/java/org/example/_11/container/with/most/water/Solution.java
@@ -1,25 +1,21 @@
 package org.example._11.container.with.most.water;
 
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
         int maxArea = 0;
-        List<Integer> leftPossibilities = new ArrayList<>(2 * height.length + 2);
-        int lastLeft = 0;
-        for (int i = 0; i < height.length; i++) {
-            if (height[i] > lastLeft) {
-                lastLeft = height[i];
-                leftPossibilities.add(i);
-                leftPossibilities.add(height[i]);
-            }
-            for (int j = 0; j < leftPossibilities.size(); j += 2) {
-                int area = (i - leftPossibilities.get(j)) * Math.min(leftPossibilities.get(j + 1), height[i]);
-                if (area > maxArea)
-                    maxArea = area;
-            }
+
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            if (area > maxArea)
+                maxArea = area;
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
         }
+
         return maxArea;
     }
 }
