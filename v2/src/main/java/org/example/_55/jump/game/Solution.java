ea744a99126e8dfae58f7c42ee8133b3f433652e
evilbc <pawel.jastrzebski01@gmail.com>
2025-03-20 20:33:17 +0100

    add 55. Jump Game

---
+++ v2/src/main/java/org/example/_55/jump/game/Solution.java
@@ -0,0 +1,14 @@
+package org.example._55.jump.game;
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] + i >= goal)
+                goal = i;
+        }
+
+        return goal == 0;
+    }
+}
