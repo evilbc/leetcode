5d37d858b3293641d2d0343e7c40756f30d1b0b8
evilbc <pawel.jastrzebski01@gmail.com>
2025-03-21 11:49:12 +0100

    optimize 198. House Robber

--- v2/src/main/java/org/example/_198/house/robber/Solution.java
+++ v2/src/main/java/org/example/_198/house/robber/Solution.java
@@ -2,22 +2,15 @@ package org.example._198.house.robber;
 
 class Solution {
     public int rob(int[] nums) {
-        return rob(nums, 0, new int[nums.length]);
-    }
+        int prev = 0;
+        int max = 0;
 
-    public int rob(int[] nums, int index, int[] cache) {
-        if (index + 1 == nums.length)
-            return nums[index];
-        if (index + 1 > nums.length)
-            return 0;
-        if (cache[index] != 0)
-            return cache[index];
+        for (int num : nums) {
+            int tmp = Math.max(max, prev + num);
+            prev = max;
+            max = tmp;
+        }
 
-        if (nums[index] >= nums[index + 1])
-            return nums[index] + rob(nums, index + 2, cache);
-
-        int result = Math.max(nums[index] + rob(nums, index + 2, cache), rob(nums, index + 1, cache));
-        cache[index] = result;
-        return result;
+        return max;
     }
 }
