cf6fc632b83701b31934d493a78033cf7c7712ed
evilbc <pawel.jastrzebski01@gmail.com>
2025-03-24 20:06:08 +0100

    simplify 122. Best Time to Buy and Sell Stock II

--- v2/src/main/java/org/example/_122/best/time/to/buy/and/sell/stock/ii/Solution.java
+++ v2/src/main/java/org/example/_122/best/time/to/buy/and/sell/stock/ii/Solution.java
@@ -2,28 +2,14 @@ package org.example._122.best.time.to.buy.and.sell.stock.ii;
 
 class Solution {
     public int maxProfit(int[] prices) {
-        int localMin = prices[0];
-        int prev = prices[0];
         int profit = 0;
-        int localProfit = 0;
-        for (int price : prices) {
-            if (price < localMin) {
-                localMin = price;
-            }
-            if (price > localMin) {
-                if (localProfit == 0) {
-                    localProfit = price - localMin;
-                } else if (price > prev) {
-                    localProfit += (price - prev);
-                } else {
-                    profit += localProfit;
-                    localProfit = 0;
-                    localMin = price;
-                }
-            }
-            prev = price;
+        for (int i = 1; i < prices.length; i++) {
+            int prev = prices[i - 1];
+            int price = prices[i];
+            int localProfit = price - prev;
+            if (localProfit > 0)
+                profit += localProfit;
         }
-        profit += localProfit;
         return profit;
     }
 }
