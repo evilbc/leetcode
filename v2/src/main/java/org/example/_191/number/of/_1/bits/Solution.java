bc014adb915e2bac3ba5fb6f5992a092825acb96
evilbc <pawel.jastrzebski01@gmail.com>
2025-03-24 14:36:47 +0100

    add 191. Number of 1 Bits

---
+++ v2/src/main/java/org/example/_191/number/of/_1/bits/Solution.java
@@ -0,0 +1,13 @@
+package org.example._191.number.of._1.bits;
+
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) == 1)
+                count++;
+            n >>= 1;
+        }
+        return count;
+    }
+}
