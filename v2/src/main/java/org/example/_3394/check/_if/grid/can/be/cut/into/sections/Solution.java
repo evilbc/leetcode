718db1ae9074b11d47b06aa2032f499af755b44e
evilbc <pawel.jastrzebski01@gmail.com>
2025-03-25 13:12:42 +0100

    optimize 3394. Check if Grid can be Cut into Sections

--- v2/src/main/java/org/example/_3394/check/_if/grid/can/be/cut/into/sections/Solution.java
+++ v2/src/main/java/org/example/_3394/check/_if/grid/can/be/cut/into/sections/Solution.java
@@ -1,70 +1,45 @@
 package org.example._3394.check._if.grid.can.be.cut.into.sections;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 class Solution {
     public boolean checkValidCuts(int n, int[][] rectangles) {
         if (rectangles.length < 3)
             return false;
 
-        List<int[]> merged = mergeHorizontally(rectangles);
-        if (merged.size() >= 3)
-            return true;
+        int[][] xs = new int[rectangles.length][2];
+        int[][] ys = new int[rectangles.length][2];
+        for (int i = 0; i < rectangles.length; i++) {
+            xs[i][0] = rectangles[i][0];
+            xs[i][1] = rectangles[i][2];
+            ys[i][0] = rectangles[i][1];
+            ys[i][1] = rectangles[i][3];
+        }
 
-        merged = mergeVertically(rectangles);
-        return merged.size() >= 3;
+        return check(xs) || check(ys);
     }
 
-    public List<int[]> mergeHorizontally(int[][] rectangles) {
-        Arrays.sort(rectangles, Comparator.comparingInt(o -> o[0]));
+    public boolean check(int[][] directions) {
+        Arrays.sort(directions, Comparator.comparingInt(o -> o[0]));
 
-        List<int[]> merged = new ArrayList<>();
-        int[] current = new int[] { rectangles[0][0], rectangles[0][2] };
+        int count = 0;
+        int prevEnd = directions[0][1];
 
-        for (int i = 1; i < rectangles.length; i++) {
-            int[] interval = rectangles[i];
+        for (int i = 1; i < directions.length; i++) {
+            int[] interval = directions[i];
             int intervalStart = interval[0];
-            int prevEnd = current[1];
 
-            if (intervalStart < prevEnd) {
-                current[1] = Math.max(current[1], interval[2]);
+            if (intervalStart >= prevEnd) {
+                if (++count >= 2)
+                    return true;
+                prevEnd = interval[1];
             } else {
-                merged.add(current);
-                current = new int[] { interval[0], interval[2] };
+                prevEnd = Math.max(prevEnd, interval[1]);
             }
 
         }
 
-        merged.add(current);
-
-        return merged;
-    }
-
-    public List<int[]> mergeVertically(int[][] rectangles) {
-        Arrays.sort(rectangles, Comparator.comparingInt(o -> o[1]));
-
-        List<int[]> merged = new ArrayList<>();
-        int[] current = new int[] { rectangles[0][1], rectangles[0][3] };
-
-        for (int i = 1; i < rectangles.length; i++) {
-            int[] interval = rectangles[i];
-            int intervalStart = interval[1];
-            int prevEnd = current[1];
-
-            if (intervalStart < prevEnd) {
-                current[1] = Math.max(current[1], interval[3]);
-            } else {
-                merged.add(current);
-                current = new int[] { interval[1], interval[3] };
-            }
-
-        }
-
-        merged.add(current);
-
-        return merged;
+        return false;
     }
 }
