fd81676b756b6cc3a9bf7549905aa5286e92fdcb
evilbc <pawel.jastrzebski01@gmail.com>
2025-03-26 10:52:49 +0100

    add 901. Online Stock Span

---
+++ v2/src/main/java/org/example/_901/online/stock/span/Solution.java
@@ -0,0 +1,29 @@
+package org.example._901.online.stock.span;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    private List<Integer> stocks;
+    private List<Integer> spans;
+
+    public StockSpanner() {
+        stocks = new ArrayList<>();
+        spans = new ArrayList<>();
+    }
+
+    public int next(int price) {
+        int count = 1;
+        for (int i = stocks.size() - 1; i >= 0; i--) {
+            if (stocks.get(i) <= price) {
+                int span = spans.get(i);
+                count += span;
+                i -= (span - 1);
+            } else
+                break;
+        }
+        stocks.add(price);
+        spans.add(count);
+        return count;
+    }
+}
