180922842f01be0a696d4f7c89226bb6dcc0223b
evilbc <pawel.jastrzebski01@gmail.com>
2025-03-26 11:02:44 +0100

    add 649. Dota2 Senate

---
+++ v2/src/main/java/org/example/_649/dota2/senate/Solution.java
@@ -0,0 +1,40 @@
+package org.example._649.dota2.senate;
+
+import org.example._637.average.of.levels.in.binary.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String predictPartyVictory(String senate) {
+        StringBuilder sb = new StringBuilder();
+        int[] bans = { 0, 0 }; // [0] - bans to Rs, [1] - bans to Ds
+        boolean[] senatorsExist = { false, false }; // [0] Rs exist, [1] - Ds exist
+        while (true) {
+            for (int i = 0; i < senate.length(); i++) {
+                char c = senate.charAt(i);
+                processSenator(bans, senatorsExist, sb, c == 'R');
+            }
+            if (!senatorsExist[0])
+                return "Dire";
+            if (!senatorsExist[1])
+                return "Radiant";
+            senatorsExist[0] = false;
+            senatorsExist[1] = false;
+            senate = sb.toString();
+            sb.setLength(0);
+        }
+    }
+
+    private static void processSenator(int[] bans, boolean[] senatorsExist, StringBuilder sb, boolean isRadiant) {
+        int partyIndex = isRadiant ? 0 : 1;
+        int oppositionIndex = isRadiant ? 1 : 0;
+        if (bans[partyIndex] > 0) {
+            bans[partyIndex]--;
+        } else {
+            bans[oppositionIndex]++;
+            senatorsExist[partyIndex] = true;
+            sb.append(isRadiant ? 'R' : 'D');
+        }
+    }
+}
