cff1ad7601af50ccc8ea145250c22c106879fd32
evilbc <pawel.jastrzebski01@gmail.com>
2025-03-27 12:59:01 +0100

    optimize 130. Surrounded Regions

--- v2/src/main/java/org/example/_130/surrounded/regions/Solution.java
+++ v2/src/main/java/org/example/_130/surrounded/regions/Solution.java
@@ -1,81 +1,65 @@
 package org.example._130.surrounded.regions;
 
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
-
 class Solution {
     private static final int[][] DIRECTIONS = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        solution.solve(new char[][] { { 'X', 'X', 'X', 'X' }, { 'X', 'O', 'O', 'X' }, { 'X', 'X', 'O', 'X' },
-                { 'X', 'O', 'X', 'X' } });
-    }
+    private static final char TMP_UNCAPTURABLE = '.';
+    private static final char TO_CAPTURE = 'O';
+    private static final char CAPTURED = 'X';
 
     public void solve(char[][] board) {
-        Set<Coordinates> visited = new HashSet<>();
+        rejectBoundaries(board);
+        captureAll(board);
+        restoreBoundaries(board);
+    }
 
-        for (int i = 1; i < board.length - 1; i++) {
-            for (int j = 1; j < board[0].length - 1; j++) {
-                if (board[i][j] == 'O' && !visited.contains(new Coordinates(i, j))) {
-                    Set<Coordinates> group = new HashSet<>();
-                    if (canCapture(board, i, j, visited, group)) {
-                        group.forEach(coords -> board[coords.x][coords.y] = 'X');
-                    }
+    private void rejectBoundaries(char[][] board) {
+        for (int x = 0; x < board.length; x++) {
+            if (board[x][0] == TO_CAPTURE) {
+                markBoundaryUncapturable(board, x, 0);
+            }
+            if (board[x][board[0].length - 1] == TO_CAPTURE) {
+                markBoundaryUncapturable(board, x, board[0].length - 1);
+            }
+        }
+
+        for (int y = 0; y < board[0].length; y++) {
+            if (board[0][y] == TO_CAPTURE) {
+                markBoundaryUncapturable(board, 0, y);
+            }
+            if (board[board.length - 1][y] == TO_CAPTURE) {
+                markBoundaryUncapturable(board, board.length - 1, y);
+            }
+        }
+    }
+
+    private void captureAll(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == TO_CAPTURE) {
+                    board[i][j] = CAPTURED;
                 }
             }
         }
     }
 
-    private boolean canCapture(char[][] board, int x, int y, Set<Coordinates> visited, Set<Coordinates> group) {
-        if (board[x][y] == 'X') {
-            return true;
-        }
-        if (x == 0 || x == board.length - 1 || y == 0 || y == board[0].length - 1) {
-            return false;
-        }
-        Coordinates coordinates = new Coordinates(x, y);
-        if (group.contains(coordinates)) {
-            return true;
-        }
-        if (visited.contains(coordinates)) {
-            return false;
-        }
-
-        group.add(coordinates);
-        visited.add(coordinates);
-        for (int[] direction : DIRECTIONS) {
-            int newX = x + direction[0];
-            int newY = y + direction[1];
-            if (!canCapture(board, newX, newY, visited, group)) {
-                return false;
+    private void restoreBoundaries(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == TMP_UNCAPTURABLE) {
+                    board[i][j] = TO_CAPTURE;
+                }
             }
         }
-
-        return true;
     }
 
-    private static class Coordinates {
-        private final int x;
-        private final int y;
-
-        public Coordinates(int x, int y) {
-            this.x = x;
-            this.y = y;
+    private void markBoundaryUncapturable(char[][] board, int x, int y) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length || board[x][y] != TO_CAPTURE) {
+            return;
         }
 
-        @Override
-        public boolean equals(Object o) {
-            if (o == null || getClass() != o.getClass())
-                return false;
-            Coordinates that = (Coordinates) o;
-            return x == that.x && y == that.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
+        board[x][y] = TMP_UNCAPTURABLE;
+        for (int[] direction : DIRECTIONS) {
+            markBoundaryUncapturable(board, x + direction[0], y + direction[1]);
         }
     }
 }
