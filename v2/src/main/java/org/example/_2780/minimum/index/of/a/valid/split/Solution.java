caa80c39e731af607f0f53257931ce212606dafb
evilbc <pawel.jastrzebski01@gmail.com>
2025-03-27 12:09:32 +0100

    add 2780. Minimum Index of a Valid Split

---
+++ v2/src/main/java/org/example/_2780/minimum/index/of/a/valid/split/Solution.java
@@ -0,0 +1,54 @@
+package org.example._2780.minimum.index.of.a.valid.split;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.minimumIndex(List.of(1,2,2,2));
+    }
+
+    public int minimumIndex(List<Integer> nums) {
+        MajorityElementResult majorityElementResult = majorityElement(nums);
+        int majorityElement = majorityElementResult.majorityElement;
+        int count = majorityElementResult.count;
+        int leftCount = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) == majorityElement) {
+                leftCount++;
+            }
+            if (2 * leftCount > i + 1 && 2 * (count - leftCount) > nums.size() - i - 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private MajorityElementResult majorityElement(List<Integer> nums) {
+        Map<Integer, Integer> counts = new HashMap<>();
+        int majorityElement = 0;
+        for (int num : nums) {
+            Integer count = counts.getOrDefault(num, 0);
+            if (++count > nums.size() / 2) {
+                majorityElement = num;
+            }
+            counts.put(num, count);
+        }
+
+        return new MajorityElementResult(majorityElement, counts.get(majorityElement));
+    }
+
+    private static class MajorityElementResult {
+        private int majorityElement;
+        private int count;
+
+        public MajorityElementResult(int majorityElement, int count) {
+            this.majorityElement = majorityElement;
+            this.count = count;
+        }
+    }
+}
