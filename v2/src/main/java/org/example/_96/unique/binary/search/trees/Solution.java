14188adeae8442d88c9881de28cf31503603461f
evilbc <pawel.jastrzebski01@gmail.com>
2025-04-12 11:54:28 +0200

    add 96. Unique Binary Search Trees

---
+++ v2/src/main/java/org/example/_96/unique/binary/search/trees/Solution.java
@@ -0,0 +1,28 @@
+package org.example._96.unique.binary.search.trees;
+
+import org.example._94.binary.tree.inorder.traversal.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int numTrees(int n) {
+        return numTrees(1, n, new int[n + 1][n + 1]);
+    }
+
+    private int numTrees(int min, int max, int[][] cache) {
+        if (min >= max)
+            return 1;
+        if (cache[min][max] != 0)
+            return cache[min][max];
+
+        int count = 0;
+        for (int i = min; i <= max; i++) {
+            count += numTrees(min, i - 1, cache) * numTrees(i + 1, max, cache);
+        }
+        cache[min][max] = count;
+        cache[max][min] = count;
+        return count;
+    }
+
+}
