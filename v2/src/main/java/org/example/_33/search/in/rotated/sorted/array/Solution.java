c1e72eda3ff9931541f969ad6e5537899069f116
evilbc <pawel.jastrzebski01@gmail.com>
2025-04-30 10:53:52 +0200

    add 33. Search in Rotated Sorted Array

---
+++ v2/src/main/java/org/example/_33/search/in/rotated/sorted/array/Solution.java
@@ -0,0 +1,24 @@
+package org.example._33.search.in.rotated.sorted.array;
+
+class Solution {
+    private static final int NOT_FOUND = -1;
+
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) return mid;
+
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) right = mid - 1;
+                else left = mid + 1;
+            } else {
+                if (target > nums[mid] && target <= nums[right]) left = mid + 1;
+                else right = mid - 1;
+            }
+        }
+        if (nums[left] == target) return left;
+        return NOT_FOUND;
+    }
+}
