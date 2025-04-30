39dc25771d7f974c127d013a50fc59ca2deff396
evilbc <pawel.jastrzebski01@gmail.com>
2025-04-30 11:06:13 +0200

    add 225. Implement Stack using Queues

---
+++ v2/src/main/java/org/example/_225/implement/stack/using/queues/MyStack.java
@@ -0,0 +1,31 @@
+package org.example._225.implement.stack.using.queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+    private final Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        queue.add(x);
+        for (int i = 1; i < queue.size(); i++) {
+            queue.add(queue.remove());
+        }
+    }
+
+    public int pop() {
+        return queue.remove();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
