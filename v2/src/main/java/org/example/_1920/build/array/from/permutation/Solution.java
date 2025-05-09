a9be0c48f64e54b74cced4381aae5a10a2ae0d85
evilbc <pawel.jastrzebski01@gmail.com>
2025-05-09 13:12:33 +0200

    add 1920. Build Array from Permutation

---
+++ v2/src/main/java/org/example/_1920/build/array/from/permutation/Solution.java
@@ -0,0 +1,13 @@
+package org.example._1920.build.array.from.permutation;
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = nums[nums[i]];
+        }
+
+        return result;
+    }
+}
