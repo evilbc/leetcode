436608d3576612e2b2dd922c1b846a9e8f128720
evilbc <pawel.jastrzebski01@gmail.com>
2025-05-27 10:59:07 +0200

    add 876. Middle of the Linked List

---
+++ v2/src/main/java/org/example/_876/middle/of/the/linked/list/Solution.java
@@ -0,0 +1,13 @@
+package org.example._876.middle.of.the.linked.list;
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
